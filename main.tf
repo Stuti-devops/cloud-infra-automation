43d0baf872fe60e9a08d407094b8f386e19688bd
STUTI <stuti.srivastava2510@gmail.com>
2025-07-06 18:44:29 +0530

    Rg added

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.34.0"
+    }
+  }
+}
+
+provider "azurerm" {
+ features{
+
+ }
+ subscription_id = "332de106-8035-4f48-a71e-790b9e30e3ff"
+}
+resource "azurerm_resource_group" "name" {
+    name ="Rg1"
+    location = "westus"
+  
+}
